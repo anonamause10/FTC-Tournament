3bc48a625ac846f4f7b41a29f0a30ab65bca1156
Alex <xia.alex.t@gmail.com>
2018-03-01 10:58:22 -0600

    woo

---
+++ Relic.java
@@ -0,0 +1,63 @@
+public class Relic implements GameElement{
+    private boolean isStanding;
+    private boolean side; //true is red
+    private int scoreZone;
+    public Relic (){
+        isStanding = false;
+        scoreZone = 0;
+    }
+    public void setStanding(boolean b){
+        isStanding = b;
+    }
+    public void setZone(int s){
+        scoreZone = s;
+    }
+    public void setSide(boolean b){
+        side = b;
+    }
+    public boolean canBeScored(int time){
+        if(time >= 120){
+            return true;
+        }
+        return false;
+    }
+    public int getPointValue(){
+        if (scoreZone == 1){
+            if(isStanding){
+                return 25;
+            }
+            return 10;
+        }
+        if (scoreZone == 2){
+            if(isStanding){
+                return 35;
+            }
+            return 20;
+        }
+        if (scoreZone == 3){
+            if(isStanding){
+                return 55;
+            }
+            return 40;
+        }
+        return 0;
+    }
+    public String toString(){
+        String s = "This relic is on the ";
+        if(side == true)
+            s += "red alliance, ";
+        else
+            s+= "blue alliance, ";
+        if(scoreZone == 0){
+            s += "and is not scored.";
+            return s;
+        }
+        else
+            s += "is scored, in zone " + scoreZone + ", ";
+        if(isStanding)
+            s += "and is standing up";
+        else
+            s += "and is not standing up";
+        return s;
+    }
+}
