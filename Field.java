56914bf258fa94c04af623312a6b3fd462e199d0
Arjun Pal <anonamause10@gmail.com>
2018-03-05 14:04:00 -0600

    Started Field

---
+++ Field.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class Field here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Field
+{
+    // instance variables - replace the example below with your own
+    private GlyphPit pit;
+    private Cryptobox redRelic;
+    private Cryptobox blueRelic;
+    private Cryptobox redOther;
+    private Cryptobox blueOther;
+    private JewelBoard redRel;
+    private JewelBoard bluRel;
+    private JewelBoard redOth;
+    private JewelBoard bluOth;
+    private Relic redR;
+    private Relic bluR;
+    private Relic redO;
+    private Relic bluO;
+    private BalanceBoard rR;
+    private BalanceBoard bR;
+    private BalanceBoard rO;
+    private BalanceBoard bO;
+    private Robot rRel;
+    private Robot bRel;
+    private Robot rOth;
+    private Robot bOth;
+    
+    
+}
--- Team.java
+++ Team.java
@@ -35,6 +35,7 @@ public class Team{
         rP = 0;
         ranking = 0;
     }
+
     public Team(Robot r, String tN, int teamN, double dL, double pL, double pitL, 
     double gPL, double oL, double eNL, double iL, double mL, double cL, double rS, 
     boolean iR){
@@ -56,61 +57,75 @@ public class Team{
         rP = 0;
         ranking = 0;
     }
+
     public double getRelationsSkill(){
         return relationsSkill;
     }
+
     public double getInspireScore(){
         return (presentationLevel * 2) + pitLevel + (graciousProfessionalismLevel * 5) +
         (outreachLevel * 3) + (designLevel * 3) + (engineeringNotebookLevel * 2);
     }
+
     public double getDesignScore(){
         return designLevel + this.getJudgeScore() * 0.2;
     }
+
     public double getThinkScore(){
         return engineeringNotebookLevel + this.getJudgeScore() * 0.2;
     }
+
     public double getConnectScore(){
         return outreachLevel + this.getJudgeScore() * 0.2;
     }
+
     public double getInnovateScore(){
         return innovateLevel + this.getJudgeScore() * 0.2;   
     }
+
     public double getMotivateScore(){
         return motivateLevel + this.getJudgeScore() * 0.2;
     }
+
     public double getControlScore(){
         return controlLevel + this.getJudgeScore() * 0.2;
     }
+
     public double getJudgeScore(){
         return (presentationLevel*2) + pitLevel;
     }
+
     public int getQP(){
         return qP;
     }
+
     public int getRP(){
         return rP;
     }
+
     public int getRanking(){
         return ranking;
     }
+
     public void setRank(int n){
         ranking = n;
     }
+
     public void setQP(int n){
         qP = n;
     }
+
     public void setRP(int n){
         rP = n;
     }
+
     public String toString(){
         String str = "Team ";
         str += teamNumber;
         str+= ": " + teamName;
-        
-        
+
         return str;
     }
 }
-
 
 
