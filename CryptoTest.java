e36a78edc970977208add96bedc5c2f2deb1b0ae
Arjun Pal <anonamause10@gmail.com>
2018-03-05 12:53:38 -0600

    GlyphPit

--- CryptoTest.java
+++ CryptoTest.java
@@ -11,19 +11,20 @@ public class CryptoTest
         Cryptobox x = new Cryptobox(true);
         Glyph w = new Glyph(1);
         Glyph b = new Glyph(0);
-        x.scoreGlyph(w,1);
         x.scoreGlyph(b,1);
         x.scoreGlyph(w,1);
+        x.scoreGlyph(w,1);
         x.scoreGlyph(b,1);
-        x.scoreGlyph(b,2);
         x.scoreGlyph(w,2);
         x.scoreGlyph(b,2);
+        x.scoreGlyph(b,2);
         x.scoreGlyph(w,2);
-        x.scoreGlyph(w,3);
         x.scoreGlyph(b,3);
+        x.scoreGlyph(w,3);
         x.scoreGlyph(w,3);
         x.scoreGlyph(b,3);
         System.out.println(x.printCiph());
         System.out.println(x.checkCipher());
+        System.out.println(x.whichCiph());
     }
 }
---
+++ GlyphPit.java
@@ -0,0 +1,47 @@
+import java.util.*;
+/**
+ * Write a description of class GlyphPit here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class GlyphPit
+{
+    private List<Glyph> whites;
+    private List<Glyph> browns;
+    
+    public GlyphPit(){
+        Glyph w = new Glyph(1);
+        Glyph b = new Glyph(0);
+        
+        for(int i = 0; i<24; i++){
+            whites.add(w);
+            
+        }
+        
+        for(int i = 0; i<24; i++){
+            browns.add(b);
+        }
+        
+    }
+    
+    public Glyph getRandGlyph(){
+        Glyph g;
+        
+        boolean x = (int)(Math.random()*2)<1;
+        if(x){
+            g = whites.remove(0);
+        }else{
+            g = browns.remove(0);
+        }
+        return g;
+    }
+    
+    public Glyph getWhiteGlyph(){
+        return whites.remove(0);
+    }
+    
+    public Glyph getBrownGlyph(){
+        return browns.remove(0);
+    }
+}
