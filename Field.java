262f33aa6054113bf99303dd135898eda449e2b3
Arjun Pal <anonamause10@gmail.com>
2018-03-05 14:07:27 -0600

    public-ed field

--- Field.java
+++ Field.java
@@ -8,27 +8,27 @@
 public class Field
 {
     // instance variables - replace the example below with your own
-    private GlyphPit pit;
-    private Cryptobox redRelic;
-    private Cryptobox blueRelic;
-    private Cryptobox redOther;
-    private Cryptobox blueOther;
-    private JewelBoard redRel;
-    private JewelBoard bluRel;
-    private JewelBoard redOth;
-    private JewelBoard bluOth;
-    private Relic redR;
-    private Relic bluR;
-    private Relic redO;
-    private Relic bluO;
-    private BalanceBoard rR;
-    private BalanceBoard bR;
-    private BalanceBoard rO;
-    private BalanceBoard bO;
-    private Robot rRel;
-    private Robot bRel;
-    private Robot rOth;
-    private Robot bOth;
+    public GlyphPit pit;
+    public Cryptobox redRelic;
+    public Cryptobox blueRelic;
+    public Cryptobox redOther;
+    public Cryptobox blueOther;
+    public JewelBoard redRel;
+    public JewelBoard bluRel;
+    public JewelBoard redOth;
+    public JewelBoard bluOth;
+    public Relic redR;
+    public Relic bluR;
+    public Relic redO;
+    public Relic bluO;
+    public BalanceBoard rR;
+    public BalanceBoard bR;
+    public BalanceBoard rO;
+    public BalanceBoard bO;
+    public Robot rRel;
+    public Robot bRel;
+    public Robot rOth;
+    public Robot bOth;
     
     
 }
