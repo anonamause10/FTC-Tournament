267ec9d7771ff92cf4c279e3327d72d0a5b4bc83
Arjun Pal <anonamause10@gmail.com>
2018-03-21 21:36:29 -0500

    new Algorithm

--- MatchListGenerator.java
+++ MatchListGenerator.java
@@ -16,52 +16,33 @@ public class MatchListGenerator
 
     public List<Team[]> generateMatches(int numPlays){
         int n = (((numPlays*teams.size())%4)==0) ? ((numPlays*teams.size())/4) : (((numPlays*teams.size())/4)+1);
-        for(int i = 0; i<n-1; i++){
-            List<Team> temp = setE(teams);
-            Team[] boiz = new Team[4];
-            for(int k = 0; k< 4; k++){
-                System.out.println(i);
-                Team t = temp.remove((int)(Math.random()*temp.size()));
-
-                while(t.getMatchesPlayed()>=numPlays){
-                    /*if(i ==n-1&&k>((numPlays*teams.size())%4)){
-                    temp.add(t);
-                    break;
-                    }*/
-                    t = temp.remove((int)(Math.random()*temp.size()));
+        for(int i = 0; i<n; i++){
+            List<Team> nDone = setE(teams);
+            List<Team> done = new ArrayList<Team>();
+            for(int o = 0; o<nDone.size(); o++){
+                if(nDone.get(o).getMatchesPlayed()>=numPlays){
+                    done.add(nDone.remove(o));
+                    o--;
                 }
+            }
+            Team[] boiz = new Team[4];
+
+            for(int k = 0; k< 4; k++){
+                System.out.println("ree");
+                Team t = new Team();
+                try{
+                    t = nDone.remove(0);
+                }catch(IndexOutOfBoundsException e){
+                    t = done.remove((int)(Math.random()*done.size()));
+                }
+
                 t.incMatches();
                 boiz[k] = t;
             }
             matches.add(boiz);
 
         }
-        List<Team> nDone = setE(teams);
-        List<Team> done = new ArrayList<Team>();
-        for(int o = 0; o<nDone.size(); o++){
-            if(nDone.get(o).getMatchesPlayed()>=numPlays){
-                done.add(nDone.remove(o));
-                o--;
-            }
-        }
-        List<Team> temp = setE(teams);
-        Team[] boiz = new Team[4];
-        for(int k = 0; k< 4; k++){
-            System.out.println("ree");
-            Team t = new Team();
-            try{
-                t = nDone.remove(0);
-            }catch(IndexOutOfBoundsException e){
-                
-                t = done.remove((int)(Math.random()*done.size()));
-            }
-            
-            
-            t.incMatches();
-            boiz[k] = t;
-        }
-        matches.add(boiz);
-
+        
         return matches;
     }
 
